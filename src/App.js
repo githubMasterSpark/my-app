c4ef757224f14bcede2e437aa0457ad7461c59f1
githubMasterSpark <bdwang@ucsd.edu>
2025-04-29 13:06:23 -0700

    yeah

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate
+} from "react-router-dom";
+import About from "./about";
 
 function MyButton() {
+  const navigate = useNavigate();
+
   const handleClick = () => {
-    alert("this is a button, which should take to another page i think");
+    navigate("/about");
   };
+
   return (
     <button onClick={handleClick}>
-      test button
+      Go to About Page
     </button>
   );
 }
 
 export default function MyApp(){
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <p>I have no idea what's going on and
-          I used the create thingamabob before realizing it was
-          depreciated???.</p>
-          <newline></newline>
-      <p>This is a working button that does not link to anything.</p>
-      <MyButton />
-    </div>
+    <Router>
+      <div>
+        <h1>Application 2</h1>
+        <p>Test to function.</p>
+        <p>This is a working button that links to the About page.</p>
+        <MyButton />
+        <Routes>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <p>This is the home page.</p>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 };
